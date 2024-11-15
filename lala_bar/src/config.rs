8d2933914dbe0d43411f24a7ee1fb170366f96f8
sundaram123krishnan <krishnsundaram@gmail.com>
2024-11-15 18:16:02 +0530

    chore: refactor

---
+++ lala_bar/src/config.rs
@@ -0,0 +1,34 @@
+use iced::widget::svg;
+use std::sync::LazyLock;
+
+pub const BEGINNING_UP_MARGIN: i32 = 10;
+
+pub const UNIT_MARGIN: i32 = 135;
+
+pub const EXTRAINF_MARGIN: i32 = BEGINNING_UP_MARGIN + 4 * UNIT_MARGIN;
+
+pub const LAUNCHER_SVG: &[u8] = include_bytes!("../asserts/launcher.svg");
+
+pub const RESET_SVG: &[u8] = include_bytes!("../asserts/reset.svg");
+
+pub const ERROR_SVG: &[u8] = include_bytes!("../asserts/error.svg");
+
+pub const GO_NEXT: &[u8] = include_bytes!("../asserts/go-next.svg");
+
+pub static GO_NEXT_HANDLE: LazyLock<svg::Handle> =
+    LazyLock::new(|| svg::Handle::from_memory(GO_NEXT));
+
+pub const GO_PREVIOUS: &[u8] = include_bytes!("../asserts/go-previous.svg");
+
+pub static GO_PREVIOUS_HANDLE: LazyLock<svg::Handle> =
+    LazyLock::new(|| svg::Handle::from_memory(GO_PREVIOUS));
+
+pub const PLAY: &[u8] = include_bytes!("../asserts/play.svg");
+
+pub static PLAY_HANDLE: LazyLock<svg::Handle> = LazyLock::new(|| svg::Handle::from_memory(PLAY));
+
+pub const PAUSE: &[u8] = include_bytes!("../asserts/pause.svg");
+
+pub static PAUSE_HANDLE: LazyLock<svg::Handle> = LazyLock::new(|| svg::Handle::from_memory(PAUSE));
+
+pub const MAX_SHOWN_NOTIFICATIONS_COUNT: usize = 4;
---
+++ lala_bar/src/notify.rs
@@ -0,0 +1,142 @@
+use crate::{LalaMusicBar, Message};
+use iced::widget::{button, column, image, markdown, row, svg, text, Space, Stack};
+use iced::{Font, Length};
+use iced_layershell::MultiApplication;
+use iced_zbus_notification::{ImageInfo, NotifyUnit};
+
+#[derive(Debug, Clone)]
+pub struct NotifyUnitWidgetInfo {
+    pub to_delete: bool,
+    pub upper: i32,
+    pub counter: usize,
+    pub inline_reply: String,
+    pub unit: NotifyUnit,
+}
+
+impl NotifyUnitWidgetInfo {
+    pub fn notify_button<'a>(&self, bar: &'a LalaMusicBar) -> iced::Element<'a, Message> {
+        let notify = &self.unit;
+        let notify_theme = if notify.is_critical() {
+            button::primary
+        } else {
+            button::secondary
+        };
+
+        let markdown_info = bar.notifications_markdown.get(&self.unit.id);
+        let text_render_text: iced::Element<Message> = match markdown_info {
+            Some(data) => markdown::view(
+                data,
+                markdown::Settings::default(),
+                markdown::Style::from_palette(bar.theme().palette()),
+            )
+                .map(Message::LinkClicked),
+            None => text(notify.body.clone())
+                .shaping(text::Shaping::Advanced)
+                .into(),
+        };
+
+        let text_render = Stack::new().push(text_render_text).push(
+            button("")
+                .style(|_theme, status| {
+                    let color = match status {
+                        button::Status::Hovered => iced::Color::new(0.118, 0.193, 0.188, 0.65),
+                        _ => iced::Color::TRANSPARENT,
+                    };
+                    button::Style {
+                        background: Some(iced::Background::Color(color)),
+                        ..Default::default()
+                    }
+                })
+                .width(Length::Fill)
+                .height(Length::Fill)
+                .on_press(Message::RemoveNotify(self.unit.id)),
+        );
+
+        match notify.image() {
+            Some(ImageInfo::Svg(path)) => button(row![
+                svg(svg::Handle::from_path(path))
+                    .height(Length::Fill)
+                    .width(Length::Fixed(70.)),
+                Space::with_width(4.),
+                column![
+                    text(notify.summery.clone())
+                        .shaping(text::Shaping::Advanced)
+                        .size(20)
+                        .font(Font {
+                            weight: iced::font::Weight::Bold,
+                            ..Default::default()
+                        }),
+                    text_render
+                ]
+            ])
+                .style(notify_theme)
+                .width(Length::Fill)
+                .height(Length::Fill)
+                .on_press(Message::RemoveNotify(self.unit.id))
+                .into(),
+            Some(ImageInfo::Data {
+                     pixels,
+                     width,
+                     height,
+                 }) => button(row![
+                image(image::Handle::from_rgba(
+                    width as u32,
+                    height as u32,
+                    pixels
+                )),
+                Space::with_width(4.),
+                column![
+                    text(notify.summery.clone())
+                        .shaping(text::Shaping::Advanced)
+                        .size(20)
+                        .font(Font {
+                            weight: iced::font::Weight::Bold,
+                            ..Default::default()
+                        }),
+                    text_render
+                ]
+            ])
+                .width(Length::Fill)
+                .height(Length::Fill)
+                .style(notify_theme)
+                .on_press(Message::RemoveNotify(self.unit.id))
+                .into(),
+            Some(ImageInfo::Png(path)) | Some(ImageInfo::Jpg(path)) => button(row![
+                image(image::Handle::from_path(path)).height(Length::Fill),
+                Space::with_width(4.),
+                column![
+                    text(notify.summery.clone())
+                        .shaping(text::Shaping::Advanced)
+                        .size(20)
+                        .font(Font {
+                            weight: iced::font::Weight::Bold,
+                            ..Default::default()
+                        }),
+                    text_render
+                ]
+            ])
+                .width(Length::Fill)
+                .height(Length::Fill)
+                .style(button::secondary)
+                .on_press(Message::RemoveNotify(self.unit.id))
+                .into(),
+            _ => button(column![
+                text(notify.summery.clone()).shaping(text::Shaping::Advanced),
+                text_render
+            ])
+                .width(Length::Fill)
+                .height(Length::Fill)
+                .style(notify_theme)
+                .on_press(Message::RemoveNotify(self.unit.id))
+                .into(),
+        }
+    }
+}
+
+#[allow(unused)]
+#[derive(Debug)]
+pub enum NotifyCommand {
+    ActionInvoked { id: u32, action_key: String },
+    InlineReply { id: u32, text: String },
+    NotificationClosed { id: u32, reason: u32 },
+}
