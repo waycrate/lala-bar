16359978ec1f0207a475961ba7fa344d318510fe
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2025-12-28 11:59:12 +0900

    chore:	do fmt

--- lala_bar/src/localize.rs
+++ lala_bar/src/localize.rs
@@ -1,6 +1,6 @@
 use i18n_embed::{
-    fluent::{fluent_language_loader, FluentLanguageLoader},
     DefaultLocalizer, LanguageLoader, Localizer,
+    fluent::{FluentLanguageLoader, fluent_language_loader},
 };
 use rust_embed::RustEmbed;
 use std::sync::LazyLock;
